797a2f99c64849a530d60ff18aecbcd86daff23e
cnagyei <cnagyei15@gmail.com>
2023-06-27 21:50:20 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,23 +1,6 @@
 #include <stdarg.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include "main.h"
-
-#pragma once
-
-#define PRINTF_STATE_NORMAL		0
-#define PRINTF_STATE_LENGTH		1
-#define PRINTF_STATE_LENGTH_SHORT	2
-#define PRINTF_STATE_LENGTH_LONG	3
-#define PRINTF_STATE_SPECIFIER		4
-
-
-#define PRINTF_LENGTH_DEFAULT		0
-#define PRINTF_LENGTH_SHORT_SHORT	1
-#define PRINTF_LENGTH_SHORT		2
-#define PRINTF_LENGTH_LONG		3
-#define PRINTF_LENGTH_LONG_LONG 	4
-
 
 /**
  * _printf - produces output according to a format
@@ -27,88 +10,35 @@
  */
 int _printf(const char *format, ...)
 {
-	int *argp = (int *)&format;
-	int state = PRINTF_STATE_NORMAL;
-	int length = 
+	va_list ap;
+	const char *p, *sval;
 
-	*argp++;
-	while (*format)
+	va_start(ap, format);
+
+	for (p = format; *p; p++)
 	{
-		switch (state)
+		if (*p != '%')
 		{
-			case PRINTF_STATE_NORMAL:
-				switch (*format)
-				{
-					case '%':
-						state = PRINTF_STATE_LENGTH;
-						break;
-					default:
-						putc(*format);
-						break;
-				}
-				break;
-
-			case PRINTF_STATE_LENGTH:
-				switch (*format)
-				{
-					case 'h':
-						length = PRINTF_LENGTH_SHORT;
-						state = PRINTF_STATE_LENGTH_SHORT;
-						break;
-					case 'l':
-						length = PRINTF_LENGTH_LONG;
-						state = PRINTF_STATE_LENGTH_LONG;
-						break;
-
-					default:
-						goto PRINTF_STATE_SPECIFIER;
-						break;
-
-				}
-				break;
-
-			case PRINTF_STATE_LENGTH_SHORT:
-				if (*format == 'h')
-				{
-					length = PRINTF_STATE_SHORT_SHORT;
-					state = PRINTF_STATE_SPECIFIER;
-				}
-				else goto PRINTF_STATE_SPECIFIER;
-				break;
-
-			case PRINTF_STATE_LENGTH_LONG:
-				if (*format == 'l')
-				{
-					length = PRINTF_STATE_LONG_LONG;
-					state = PRINTF_STATE_SPECIFIER;
-				}
-			
-			else goto PRINTF_STATE_SPECIFIER;
-			break;
-
-			case PRINTF_STATE_SPECIFIER:
-			PRINTF_STATE_SPECIFIER;
-			switch (*format)
-			{
-				case 'c':
-					putc((char)*argp);
-					argp++;
-					break;
-
-				case 's':
-					putstr(*(char**)argo);
-					argp++;
-					break;
-
-				case '%':
-					putc('%');
-					break;
-					
-					
-			}
-
+			_putchar(*p);
+			continue;
 		}
+		
+		switch (*++p)
+		{
+			case 'c':
+				_putchar(va_arg(ap, int));
+				break;
 
-		format++;
+			case 's':
+				for (sval = va_arg(ap, char *); *sval; sval++)
+					_putchar(*sval);
+				break;
+
+			default:
+				_putchar(*p);
+				break;
+		}
 	}
+	va_end(ap);
+	return (p - format);
 }
--- main.h
+++ main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void putc(char c);
+int _putchar(char c);
 int _printf(const char *format, ...);
-int _strlen(char *s);
 
 #endif /* MAIN_H */
